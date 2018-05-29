7435467e063071b144759d5cf82b2c90b2f8ddab
Guillaume <groult_guigui@hotmail.com>
2018-05-29 15:55:20 +0200

    Ajout des fichier index.php main.js style.css

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Forum Pirate</title>
+	<link href="style.css" rel="stylesheet">
+</head>
+<body>
+	<h1>YoHOHO!! A l'abordage Mousaillons!</h1>
+	
+	  <script src="main.js"></script>
+</body>
+</html>
