2d3a82735d494c0d1ffc4cec87f8862cb58a338d
Florian <florian.goasdoue@hotmail.com>
2018-06-06 13:02:36 +0200

    modification index.php, ajout wireframe-forum

---
+++ views/home.php
@@ -0,0 +1,143 @@
+	<body>
+
+		<div class="col full-height">
+			<div class="row navbar">
+				<div class="col"></div>
+				<div class="col faux-rhum">
+					<!-- Left to right -->
+					<div class="row">
+						<div class="col-sm-6">
+
+							<!-- normal -->
+							<div class="ih-item circle effect18 left_to_right"><a href="#">
+								<div class="img"><h3>Faux-Rhum</h3></div>
+								<div class="info">
+									<div class="info-back">
+										<img src="img/Boutons/pourparler.jpeg" alt="img">
+									</div>
+								</div>
+							</a>
+						</div>
+						<!-- end normal -->
+
+					</div>
+				</div>
+				<!-- end Left to right -->
+			</div>
+			<div class="col"></div>
+			<div class="col inscription">
+				<!-- Left to right -->
+				<div class="row">
+					<div class="col-sm-6">
+
+						<!-- normal -->
+						<div class="ih-item circle effect18 left_to_right"><a href="/inscription">
+							<div class="img"><h3>Inscription</h3></div>
+							<div class="info">
+								<div class="info-back">
+									<img src="img/Boutons/plume.png" alt="img">
+								</div>
+							</div>
+						</a>
+					</div>
+					<!-- end normal -->
+
+				</div>
+			</div>
+			<!-- end Left to right -->
+		</div>
+		<div class="col"></div>
+		<div class="col profil">
+			<!-- Left to right -->
+			<div class="row">
+				<div class="col-sm-6">
+
+					<!-- normal -->
+					<div class="ih-item circle effect18 left_to_right"><a href="#">
+						<div class="img"><h3>Profil</h3></div>
+						<div class="info">
+							<div class="info-back">
+								<img src="img/Boutons/jollyroger.png" alt="img">
+							</div>
+						</div>
+					</a>
+				</div>
+				<!-- end normal -->
+			</div>
+		</div>
+		<!-- end Left to right -->
+	</div>
+	<div class="col"></div>
+	<div class="col mp">
+		<!-- Left to right -->
+		<div class="row">
+			<div class="col-sm-6">
+				<!-- normal -->
+
+				<div class="ih-item circle effect18 left_to_right">
+					<a href="#">
+						<div class="img">								
+							<h3>Messages Privés</h3>
+						</div>
+						<div class="info">
+							<div class="info-back">
+								<img src="img/Boutons/bouteille.png" alt="img" class="bouteille">
+							</div>
+						</div>
+					</a>
+				</div>
+				<!-- end normal -->
+			</div>
+		</div>
+		<!-- end Left to right -->
+	</div>
+	<div class="col"></div>
+</div>
+	<!-- NavBar ~~ Bootstrap 
+	<nav class="navbar navbar-expand-lg navbar-dark">
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+		</button>
+		<div class="collapse navbar-collapse" id="navbarText">
+			<ul class="navbar-nav mr-auto">
+				<li class="nav-item active">
+					<a class="nav-link" href="#">Faux Rhum <span class="sr-only">(current)</span></a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="#">Inscription</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="#">Profil</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="#">Messages Privés</a>
+				</li>
+			</ul>
+			<span class="navbar-text">
+				Yooho Yohooo et une bouteille de rhum!
+			</span>
+		</div>
+	</nav>
+	~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~-->
+
+	<div class="row quinte">
+		<div class="col accueil-img">
+		</div>
+		<div class="col">
+			<div id="message-accueil-container" class="parchemin">
+				<div id="message-accueil" class="fade-in-text">
+					<h3>Bienvenue sur le Faux-Rhum</h3>
+					<p>
+						Yohoohoo Mousaillons! Larguez les amarres et ouvrez les écoutilles car nous partons pour l'aventure! C'est le moment d'aiguisez votre crochet et de sortir votre plus belle jambe de bois, ici pas de place pour les marins d'eaux douces!
+					</p>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
+	<script src="main.js"></script>
+</body>
