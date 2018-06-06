960fe2ecf6d5e9c2ee8d76f725ef4fe3516b8026
Guillaume <groult_guigui@hotmail.com>
2018-06-06 10:50:05 +0200

    Amélioration visuelle de index.php

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
 	<title>Forum Pirate</title>
 	<link href="style.css" rel="stylesheet">
 	<link href="ihover/src/ihover.css" rel="stylesheet">
+	<link rel="shortcut icon" href="img/pirate-icon.png">
 	<!-- Bootstrap CSS 
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">  -->
 	</head>
@@ -12,7 +13,7 @@
 
 		<div class="col full-height">
 			<div class="row navbar">
-				<div class="col double"></div>
+				<div class="col"></div>
 				<div class="col faux-rhum">
 					<!-- Left to right -->
 					<div class="row">
@@ -20,11 +21,10 @@
 
 							<!-- normal -->
 							<div class="ih-item circle effect18 left_to_right"><a href="#">
-								<div class="img"><img src="img/Boutons/forum.png" alt="img"></div>
+								<div class="img"><h3>Faux-Rhum</h3></div>
 								<div class="info">
 									<div class="info-back">
-										<h3>Faux-Rhum</h3>
-										<p></p>
+										<img src="img/Boutons/pourparler.jpeg" alt="img">
 									</div>
 								</div>
 							</a>
@@ -35,18 +35,18 @@
 				</div>
 				<!-- end Left to right -->
 			</div>
+			<div class="col"></div>
 			<div class="col inscription">
 				<!-- Left to right -->
 				<div class="row">
 					<div class="col-sm-6">
 
 						<!-- normal -->
-						<div class="ih-item circle effect18 left_to_right"><a href="/Sign-in">
-							<div class="img"><img src="img/Boutons/plume.png" alt="img"></div>
+						<div class="ih-item circle effect18 left_to_right"><a href="/inscription">
+							<div class="img"><h3>Inscription</h3></div>
 							<div class="info">
 								<div class="info-back">
-									<h3>Sign-in</h3>
-									<p></p>
+									<img src="img/Boutons/plume.png" alt="img">
 								</div>
 							</div>
 						</a>
@@ -57,6 +57,7 @@
 			</div>
 			<!-- end Left to right -->
 		</div>
+		<div class="col"></div>
 		<div class="col profil">
 			<!-- Left to right -->
 			<div class="row">
@@ -64,11 +65,10 @@
 
 					<!-- normal -->
 					<div class="ih-item circle effect18 left_to_right"><a href="#">
-						<div class="img"><img src="img/Boutons/jollyroger.png" alt="img"></div>
+						<div class="img"><h3>Profil</h3></div>
 						<div class="info">
 							<div class="info-back">
-								<h3>Profil</h3>
-								<p></p>
+								<img src="img/Boutons/jollyroger.png" alt="img">
 							</div>
 						</div>
 					</a>
@@ -78,6 +78,7 @@
 		</div>
 		<!-- end Left to right -->
 	</div>
+	<div class="col"></div>
 	<div class="col mp">
 		<!-- Left to right -->
 		<div class="row">
@@ -86,11 +87,12 @@
 
 				<div class="ih-item circle effect18 left_to_right">
 					<a href="#">
-						<div class="img"><img src="img/Boutons/pourparler.jpeg" alt="img"></div>
+						<div class="img">								
+							<h3>Messages Privés</h3>
+						</div>
 						<div class="info">
 							<div class="info-back">
-								<h3>Messages Privés</h3>
-								<p></p>
+								<img src="img/Boutons/bouteille.png" alt="img" class="bouteille">
 							</div>
 						</div>
 					</a>
@@ -100,7 +102,7 @@
 		</div>
 		<!-- end Left to right -->
 	</div>
-	<div class="col double"></div>
+	<div class="col"></div>
 </div>
 	<!-- NavBar ~~ Bootstrap 
 	<nav class="navbar navbar-expand-lg navbar-dark">
@@ -132,19 +134,22 @@
 	<div class="row quinte">
 		<div class="col accueil-img">
 		</div>
-		<div class="col parchemin">
-			<h3>Bienvenue sur le Faux-Rhum</h3>
-			<p id="message-accueil">
-				Yohoohoo Mousaillons! Larguez les amarres et ouvrez les écoutilles car nous partons pour l'aventure! C'est le moment d'aiguisez votre crochet et de sortir votre plus belle jambe de bois, ici pas de place pour les marins d'eaux douces!
-			</p>
+		<div class="col">
+			<div id="message-accueil-container" class="parchemin">
+				<div id="message-accueil" class="fade-in-text">
+					<h3>Bienvenue sur le Faux-Rhum</h3>
+					<p>
+						Yohoohoo Mousaillons! Larguez les amarres et ouvrez les écoutilles car nous partons pour l'aventure! C'est le moment d'aiguisez votre crochet et de sortir votre plus belle jambe de bois, ici pas de place pour les marins d'eaux douces!
+					</p>
+				</div>
+			</div>
 		</div>
 	</div>
-</div>
 
-<!-- jQuery first, then Popper.js, then Bootstrap JS -->
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
-<script src="main.js"></script>
+	<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
+	<script src="main.js"></script>
 </body>
 </html>
