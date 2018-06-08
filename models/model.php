5fbb88dd22d21bc2929a547a75c782313f64ad19
VanishaB <vanishabhujun@hotmail.fr>
2018-06-08 10:03:44 +0200

    modif

--- models/model.php
+++ models/model.php
@@ -4,16 +4,15 @@
 class Model{
 
 private $user = "root";
-private $password= "simplonco";
-// private $password= "";
+private $password="simplonco";
+//private $password= "";
 private $dbname= "forum_pirate";
 private $host= "localhost";
-private $port= 3306;
+private $port= 8088;
 
 public function connect(){
   try{
     $db= new PDO ("mysql:host=".$this->host.";dbname=".$this->dbname,$this->user,$this->password);
-    echo "connection";
     return $db;
   }
   catch(PDOException $e) {
