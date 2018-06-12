4e6a0ee682c2d9dc92d634068b556dae32336c14
SarahBen22 <sa.tahiris@gmail.com>
2018-06-12 17:56:34 +0100

    modification du controller inscription et vue profil

--- controllers/profil.php
+++ controllers/profil.php
@@ -5,6 +5,7 @@ require_once "models/utilisateurs.php";
 $profil =new UtilisateursModel(['']);
 //$ProfilListView= $profil->getById();
 
+require_once "views/navbar.php";
 require_once "views/profil.php";
 
  ?>
